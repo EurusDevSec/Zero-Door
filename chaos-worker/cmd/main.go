1c16f5c36609b12dd18179df444a74d8b177a743
LeganSar44 <hoangslevan@gmail.com>
2026-02-06 15:47:23 +0700

    refactor: Comment out unused variables and print statements in main function

--- chaos-worker/cmd/main.go
+++ chaos-worker/cmd/main.go
@@ -1,17 +1,35 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello ")
+	// fmt.Println("Hello ")
 
-	var name string = "Nemesis"
+	// var name string = "Nemesis"
 
-	var name2 = "Violet"
+	// var name2 = "Violet"
 
-	name3 := "Mikasa"
+	// name3 := "Mikasa"
 
-	fmt.Println(name)
-	fmt.Println(name2)
-	fmt.Println(name3)
+	// fmt.Println(name)
+	// fmt.Println(name2)
+	// fmt.Println(name3)
+
+	const MaxRetries = 3
+	const (
+		StatusRunning = "Running"
+		StatusFailed  = "failed"
+	)
+	// kHoi tao chua co gia tri
+	// 	var age int = 25
+	// 	var price float64 = 19.99
+	// 	var count uint = 100
+
+	// 	var name string = "Hephaetus"
+
+	// 	var isActive bool = true
+
+	// 	var i int
+	// 	var f float64
+	// 	var s string
+	// 	var b bool
+
 }
